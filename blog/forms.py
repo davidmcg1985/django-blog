86ecb6228a2823c3a7ef646f4d750b086b89f3b4
davidmcg <dmcgweb101@gmail.com>
2015-11-01 19:13:04 +0000

    added comments section for posts

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,15 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
 
 class PostForm(forms.ModelForm): # PostForm is the name of the form.
 
 	class Meta:
 		model = Post
 		fields = ('title', 'text',)
+
+
+class CommentForm(forms.ModelForm):
+
+	class Meta:
+		model = Comment
+		fields = ('author', 'text',)
